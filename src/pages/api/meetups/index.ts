9e44aa5f8e4116de54493ddb93845dc8f2110307
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-06-02 23:03:08 +0530

    Connecting & Querying a MongoDB Databas

--- src/pages/api/meetups/index.ts
+++ src/pages/api/meetups/index.ts
@@ -1,9 +1,23 @@
-function handleApiMeetups(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handleApiMeetups(req, res) {
   switch (req.method) {
     case "POST":
-      const { title, image, address, description } = req.body;
+      const data = req.body;
 
-      return;
+      const client = await MongoClient.connect(process.env.MONGODB_URI);
+      const db = client.db();
+
+      const meetupsCollection = db.collection("meetups");
+
+      const result = await meetupsCollection.insertOne(data);
+      console.log(result);
+
+      client.close();
+
+      return res.status(201).json({
+        message: "Meetup Inserted!",
+      });
 
     default:
       return res.status(400).json({
