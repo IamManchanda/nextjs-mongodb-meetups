cca23171a32a23236ccbc3e2143c706be0cd717f
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-06-02 17:09:50 +0530

    Data Fetching For Page Generation!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import MeetupItemsList from "../components/meetup-items-list/index";
 import DUMMY_MEETUPS from "../fixtures/dummy-meetups";
 
-function PageIndex() {
-  return <MeetupItemsList meetups={DUMMY_MEETUPS} />;
+function PageIndex({ meetups }) {
+  return <MeetupItemsList meetups={meetups} />;
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 1,
+  };
 }
 
 export default PageIndex;
